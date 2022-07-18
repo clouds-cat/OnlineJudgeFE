93ea4b29ac2329cc0e815b54dbc70f02c9f5c0ec
xlzzzz <13960959677>
2022-07-18 20:20:35 +0800

    front

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from "./router";
 import store from "./store"
 
+import ElementPlus from 'element-plus';
+
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 const app = createApp(App)
 
 app
+    .use(ElementPlus, { size: 'small', zIndex: 3000 })
     .use(router)
     .use(store)
     .use(Antd)
--- src/views/oj/index.vue
+++ src/views/oj/index.vue
@@ -1,30 +1,111 @@
 <template>
-  <a-result title="还没有页面!">
-    <template #icon>
-      <smile-twoTone />
-    </template>
-    <template #extra>
-      <a-button type="primary" @click="this.$router.push('/eqweqsddad')">go 404</a-button>
-    </template>
-  </a-result>
+  <body>
+    <header>
+      <div class="logo">{{ "OnlineJudge" }}</div>
+      <ul>
+        <li>{{ "题目" }}</li>
+      </ul>
+    </header>
+    <section class="banner1"></section>
+    <section class="banner2">
+      <el-card  :shadow="always" class="box-card">
+          
+          <div>111</div>
+      </el-card>
+    </section>
+  </body>
 </template>
 
 <script>
-import { SmileTwoTone } from '@ant-design/icons-vue';
+import { SmileTwoTone } from "@ant-design/icons-vue";
+
 export default {
   name: "index",
-  components:{
+  components: {
     SmileTwoTone,
   },
-  data(){
-    return {
-      
-    }
+  data() {
+    return {};
   },
 
-}
+};
+
+window.addEventListener("scroll",()=>{
+    let header=document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY>300)
+  })
 </script>
 
 <style scoped>
+body {
+  min-height: 200vh;
+  background-color: rgb(243, 243, 243);
+}
 
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 40px 100px;
+  z-index: 1;
+  transition: 0.6s;
+}
+
+.logo {
+  font-size: 32px;
+  color: #fff;
+  font-weight: 700;
+  text-decoration: none;
+  letter-spacing: 2px;
+  transition: 0.6s;
+}
+header ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header ul li {
+  list-style: none;
+  margin: 0 15px;
+  text-decoration: none;
+  color: #fff;
+  font-weight: 500;
+  letter-spacing: 2px;
+  transition: 0.6s;
+}
+.banner1 {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background: url("../../assets/1.jpg") no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
+
+.banner2 {
+  position: relative;
+  top:-100px;
+}
+
+header.sticky {
+  padding: 6px 100px;
+  background-color: #fff;
+}
+header.sticky .logo,
+header.sticky ul li{
+  color:black;
+}
+
+.box-card {
+  width: 90%;
+  height: 100vh;
+  background-color: #fff;
+  position:absolute;
+  left: 5%;
+}
 </style>
